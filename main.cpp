5fcd8613038594788abd44003a3882285ddd35e9
shanjingongzi <841655282@qq.com>
2021-12-22 23:25:57 +0800

    add main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Vector.h"
+using namespace std;
+
+int main()
+{
+    Vector vec;
+    vec.x=1;
+    vec.y=1;
+    vec.z=1;
+    cout<<"Hello Taurus Engine"<<endl;
+}
